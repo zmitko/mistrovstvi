ebad1c65418a2936ebdf62ab755e904bf8e534a5
Jiri Falta <jirkafalta@gmail.com>
2019-10-07 09:19:46 +0200

    cv03
    Mistrovství!

---
+++ Masterdetail/app/src/main/java/com/example/master_detail/AndroidInfo.java
@@ -0,0 +1,20 @@
+package com.example.master_detail;
+
+public class AndroidInfo {
+    public static final String[] NAZVY =
+            {
+                    "1.5 Cupcake",
+                    "1.6 Donut",
+                    "2.0 Eclair",
+                    "2.2 Froyo",
+                    "2.3 Gingerbread"
+            };
+    public static final String[] POPISY =
+            {
+                    "(malý muffin v košíčku so šľahačkou – 30.4.2009) ...",
+                    "(šiška s otvorom – 1.9.2009) ...",
+                    "(podlhovastý zákusok s plnkou a čokoládou 26.10.2009) ...",
+                    "(šľahačkový špic obložený ovocím – 20.5.2010)...",
+                    "(perník – 6.12.2010)..."
+            };
+}
