0f89a19f3e456028a187d1b25432f3c41f2c558d
Martin Ďuriš <martin.duris@email.cz>
2019-10-07 09:26:50 +0200

    cv03

--- app/src/main/java/com/example/cyklusaktivity/StateChangeActivity.java
+++ app/src/main/java/com/example/cyklusaktivity/StateChangeActivity.java
@@ -1,4 +1,5 @@
 package com.example.cyklusaktivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,73 +17,14 @@ public class StateChangeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.state_change_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action",
-                        Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        setContentView(R.layout.activity_main);
+// skry bočný panel
+        int screenOrientation = getResources().getConfiguration().orientation;
+        if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            View sidePane = findViewById(R.id.bocny_panel);
+            if (sidePane.getVisibility() == View.VISIBLE) {
+                sidePane.setVisibility(View.GONE);
             }
-        });
-        Log.i(LAD, "OnCreate");
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(LAD, "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(LAD, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(LAD, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(LAD, "onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(LAD, "onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(LAD, "onDestroy");
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.i(LAD, "onSaveInstanceState");
-        final EditText textBox =(EditText) findViewById(R.id.editText);
-        CharSequence sText = textBox.getText();
-        outState.putCharSequence("UlozenyText", sText);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i(LAD, "onRestoreInstanceState");
-        final EditText textBox = (EditText) findViewById(R.id.editText);
-        CharSequence sText = savedInstanceState.getCharSequence("UlozenyText");
-        textBox.setText(sText);
+        }
     }
 }
