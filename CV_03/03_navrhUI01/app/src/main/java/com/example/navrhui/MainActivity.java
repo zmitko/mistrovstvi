cf2bb2225de8ecb399185099874f497cd2c23d6e
Karel Koudelka <k.koudelka01@gmail.com>
2019-10-21 08:27:04 +0200

    cv03

---
+++ CV_03/03_navrhUI01/app/src/main/java/com/example/navrhui/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.navrhui;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        // Objekt na interakciu fragmentov v rámci akticity
+        FragmentManager fragmentManager = getFragmentManager();
+        // beginTransaction() povoli operácie s fragmentmi
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        //zistenie orientacie zariadenai
+        Configuration configInfo = getResources().getConfiguration();
+        //podľa orientácie vlož správny fragment
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            FragmentLandscape fragmentLandscape = new FragmentLandscape();
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentLandscape);
+        } else {
+            FragmentPortrait fragmentPortrait = new FragmentPortrait();
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentPortrait);
+        }
+        // nahraď fragment tak rýchlo ako je to možné
+        fragmentTransaction.commit();
+        //setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ CV_03/03_navrhUI03/app/src/main/java/com/example/navrhui3/FragmentDetail.java
@@ -0,0 +1,18 @@
+package com.example.navrhui3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentDetail extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.detail_fragment, container, false);
+        return view;
+    }
+}
+
