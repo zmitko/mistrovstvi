f5d0f78f6e15b81c2e7683c1315353f431bb5efb
Jiri Falta <jirkafalta@gmail.com>
2019-11-02 15:04:33 +0100

    cv04

---
+++ Implicitnintenty/app/src/main/java/com/example/implicitnintenty/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.implicitnintenty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.albatrosmedia.cz"));
+        startActivity(intent);
+    }
+
+}
