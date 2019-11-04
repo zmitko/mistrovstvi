27dfe2e1b9589efc2e5153e21e5eaab2fdde5646
Jan Kollert <koller.honzik@gmail.com>
2019-11-04 08:53:48 +0100

    cv04

---
+++ 04/01/app/src/main/java/com/studio/tiny/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.studio.tiny.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent iMeno = new Intent(this, druhaActivita.class);
+// očakávame že aktivita vrátihodnotu
+        final int result = 1;
+        iMeno.putExtra("volajúcaAktivita", "MainActivity");
+        startActivityForResult(iMeno, result);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView tvMeno = (TextView) findViewById(R.id.meno_od_volanej_aktivity);
+        String sMeno = data.getStringExtra("ZadaneMeno");
+        tvMeno.append(" " + sMeno);
+    }
+
+}
---
+++ 04/01/app/src/main/java/com/studio/tiny/myapplication/druhaActivita.java
@@ -0,0 +1,38 @@
+package com.studio.tiny.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class druhaActivita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//vyber layout
+        setContentView(R.layout.activity_druha_activita);
+//intent ktorý otvoril túto aktivitu
+        Intent iVolajucejAktivity = getIntent();
+// načítanie odoslaných údajov
+        String previousActivity =
+                iVolajucejAktivity.getExtras().getString("volajúcaAktivita");
+        TextView callingActivityMessage = (TextView)
+                findViewById(R.id.sprava_od_aktivity);
+        callingActivityMessage.append(" " + previousActivity);
+    }
+    public void onPosliMeno(View view) {
+// mano z prvku EditText
+        EditText usersNameET = (EditText) findViewById(R.id.zadane_meno);
+        String usersName = String.valueOf(usersNameET.getText());
+// intent pre návrat hodnoty volajúcej aktivite
+        Intent goingBack = new Intent();
+        goingBack.putExtra("ZadaneMeno", usersName);
+        setResult(RESULT_OK, goingBack);
+        finish();
+    }
+
+}
