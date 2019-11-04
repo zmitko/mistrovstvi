e47590524c79e23f99ca0c38fcad0ddea623f551
Karel Koudelka <k.koudelka01@gmail.com>
2019-11-04 08:53:33 +0100

    cv04

---
+++ CV_04/02/app/src/main/java/com/cp/cviceninulactyri/MainActivity.java
@@ -0,0 +1,24 @@
+package com.cp.cviceninulactyri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.albatrosmedia.cz"));
+        startActivity(intent);
+    }
+}
---
+++ MasterDetail/app/src/main/java/com/cp/masterdetail/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cp.masterdetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
