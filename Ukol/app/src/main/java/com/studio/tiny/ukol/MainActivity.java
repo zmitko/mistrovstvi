e85e7daa59802147d3e076bfb4c75dfb5ac27544
Jan Kollert <koller.honzik@gmail.com>
2019-12-15 11:40:57 +0100

    Úkol - nastavení

---
+++ Ukol/app/src/main/java/com/studio/tiny/ukol/MainActivity.java
@@ -0,0 +1,19 @@
+package com.studio.tiny.ukol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
