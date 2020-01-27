bdc26cfef1e2c7378fd92fb662264ad652d7cc99
Filip <c>
2020-01-27 08:30:47 +0100

    cv11
    11_Komunikace

---
+++ cv11/bluetoothKomunikace/app/src/main/java/com/example/bluetoothKomunikace/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bluetoothKomunikace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3, b4;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        BA = BluetoothAdapter.getDefaultAdapter();
+        lv = (ListView) findViewById(R.id.listView);
+    }
+    public void on(View v) {
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Zapnutý",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Už je zapnutý",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+    public void off(View v) {
+        BA.disable();
+        Toast.makeText(getApplicationContext(), "Vypnutý",
+                Toast.LENGTH_LONG).show();
+    }
+    public void visible(View v) {
+        Intent getVisible = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+    }
+    public void list(View v) {
+        pairedDevices = BA.getBondedDevices();
+        ArrayList list = new ArrayList();
+        for (BluetoothDevice bt: pairedDevices)
+            list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Zobrazujem párované zariadenia",
+                Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new
+                ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+    }
+}
+
