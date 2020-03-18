cfaf9d754faa202fe783654390d36b6efb28b030
Michal Špitálský <spitmi@delta-skola.cz>
2020-03-18 12:42:33 +0100

    cv15

---
+++ NositelnaZarizeni/app/src/main/java/com/example/nositelnazarizeni/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nositelnazarizeni;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity extends Activity {
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+}
